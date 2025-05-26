11ff9354ae5fdd29ff0f3d46e7b2937bc9f77d36
William Rodrigues <86368140+Bobonimo111@users.noreply.github.com>
2025-05-26 20:57:41 -0300

    Delete src directory

--- src/main/java/br/com/estacionamento/repositories/EstacionamentoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.estacionamento.repositories;
-
-import br.com.estacionamento.model.Estacionamento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstacionamentoRepository extends JpaRepository<Estacionamento,Long> {
-    Estacionamento findByNome(String nome);
-}
